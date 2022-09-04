702b2492d91e0e50f89d75c85d24281ebe188659
Fabricio Tanjoni <fabriciotanjoni@gmail.com>
2022-09-04 20:49:20 -0300

    Caderno ex. aula 7 md 1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace md07_CadernoExercicios_01
+{
+    class Program
+    {/*
+      1. Escreva um programa com uma função que solicita a digitação do salário atual
+        de um funcionário e o índice de reajuste. Exiba o salário reajustado.
+
+      */
+        public static double sal, indice;
+       
+        public static void CalcularNovoSalario(double newSal, double indic)
+        {
+            double nvSal = newSal * indic;
+            Console.WriteLine("\nO novo salário do funcionário, corresponde ao montante: R$ {0}", nvSal.ToString("0.00"));
+        }
+
+        public static void SolicitaSalarioeIndice()
+        {
+            Console.WriteLine("Digite o Salário do Funcionário:");
+            sal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nInforme o índice para cáluclo do novo salário:");
+            indice = Convert.ToDouble(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            SolicitaSalarioeIndice();
+            CalcularNovoSalario(sal, indice);
+            Console.WriteLine("\nFim!");
+
+        }
+    }
+}
