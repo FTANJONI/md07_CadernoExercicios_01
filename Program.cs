81b71018eef0d7a4e551545c312eefd3c67b8611
Fabricio Tanjoni <fabriciotanjoni@gmail.com>
2022-09-04 21:03:20 -0300

    Retorn version origin

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ namespace md07_CadernoExercicios_01
 {
     class Program
     {/*
-      * teste commit
       1. Escreva um programa com uma função que solicita a digitação do salário atual
         de um funcionário e o índice de reajuste. Exiba o salário reajustado.
 
@@ -28,7 +27,7 @@ namespace md07_CadernoExercicios_01
         {
             SolicitaSalarioeIndice();
             CalcularNovoSalario(sal, indice);
-            Console.WriteLine("\nFim!!!");
+            Console.WriteLine("\nFim!");
 
         }
     }
